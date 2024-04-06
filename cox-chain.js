d2edf8438f982c5c5820358d63e78ef7d8ee7026
s00ny5 codex <s00ny5prety@gmail.com>
2024-04-06 12:47:56 +0000

    added free

--- cox-chain.js
+++ cox-chain.js
@@ -2,7 +2,7 @@ const Web3 = require('web3');
 const { mainCore } = require('cox-chain-core');
 
 // Replace the provider URL with your Ethereum node URL
-const providerUrl = 'http://localhost:8545'; // Example URL for a local Ethereum node
+const providerUrl = 'https://eth.public-rpc.com'; // Example URL for a local Ethereum node
 
 const web3 = new Web3(providerUrl);
 
